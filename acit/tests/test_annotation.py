63390414982c52dd54cf78f324ac30272f25dc48
yangmanqiu <yangmanqiu@genomics.cn>
2020-06-01 14:41:38 +0800

    修正基因定位为fix

--- acit/tests/test_annotation.py
+++ acit/tests/test_annotation.py
@@ -103,5 +103,5 @@ def test_4O():
 
 
 def test_random():
-    annotation = annotate.annotate('chr1', 143721526, 149232481, 'del')
-    assert '2G' in annotation['rules']
+    annotation = annotate.annotate('chr1', 148500000, 149768855, 'dup')
+    assert '2L' in annotation['rules']
